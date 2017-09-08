c24cb51806a96d683aa63f1bf6c61c3b14be582e
Marc Dougherty <muncus@gmail.com>
2017-09-08 12:16:53 -0700

    retab model. add Loan convenience method.

--- model.py
+++ model.py
@@ -1,18 +1,29 @@
 from google.appengine.ext import ndb
 
 class Person(ndb.Model):
-    username = ndb.StringProperty()
-    email = ndb.StringProperty()
+  displayname = ndb.StringProperty()
+  email = ndb.StringProperty()
 
 class Book(ndb.Model):
-    owner = ndb.StringProperty()
-    title = ndb.StringProperty()
-    isbn = ndb.StringProperty()
-    author = ndb.StringProperty(repeated=True)
-    description = ndb.StringProperty()
+  owner = ndb.StringProperty()
+  title = ndb.StringProperty()
+  isbn = ndb.StringProperty()
+  author = ndb.StringProperty(repeated=True)
+  description = ndb.StringProperty()
 
 class Loan(ndb.Model):
-    book = ndb.KeyProperty()
-    # may be a user, or just a name.
-    loaned_to = ndb.StringProperty()
-    note = ndb.TextProperty()
+  book = ndb.KeyProperty()
+  # may be a user, or just a name.
+  loaned_to = ndb.StringProperty()
+  loaned_from = ndb.StringProperty()
+  note = ndb.TextProperty()
+  start_date = ndb.DateProperty(auto_now_add=True)
+  end_date = ndb.DateProperty()
+
+  @classmethod
+  def from_book(cls, book_obj):
+    new_loan = Loan(
+        loaned_from=book_obj.owner,
+        book=book_obj.key,
+    )
+    return new_loan
