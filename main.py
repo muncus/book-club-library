4c679dc406f2786cee3b1fba75bbbcca2b4d3628
Marc Dougherty <muncus@gmail.com>
2017-09-09 13:54:05 -0700

    Several fixes.
    Improves borrow flow, allows isbn or key in the url
    only show borrow links for books with no outstanding loans.

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from urllib import quote, urlencode, urlopen
 from flask import Flask, render_template, request, redirect, url_for
 
 BASEURL='http://library.muncustest.appspot.com'
+ADD_SCAN_REDIR = quote(BASEURL + '/add/{CODE}')
+BORROW_SCAN_REDIR = quote(BASEURL + '/borrow/{CODE}')
+
 RESULTS_PER_PAGE=40
 
 app = Flask(__name__)
@@ -23,8 +26,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('home.html',
-      add_url=quote(BASEURL + '/add/{CODE}'),
-      loan_url=quote(BASEURL + '/borrow/{CODE}'))
+      add_url=ADD_SCAN_REDIR,
+      loan_url=BORROW_SCAN_REDIR,)
 
 @app.route('/add/<isbn>', methods=['POST'])
 def add_from_form(isbn):
@@ -42,6 +45,8 @@ def add_from_form(isbn):
         'book_edit.html',
         book=book,
         msg="Changes saved.",
+        add_url=ADD_SCAN_REDIR,
+        loan_url=BORROW_SCAN_REDIR,
     )
   else:
     new_book = model.Book()
@@ -57,6 +62,8 @@ def add_from_form(isbn):
         'book_edit.html',
         book=new_book,
         msg="Book Created!",
+        add_url=ADD_SCAN_REDIR,
+        loan_url=BORROW_SCAN_REDIR,
     )
 
 @app.route('/add/<isbn>')
@@ -92,7 +99,8 @@ def add_from_isbn(isbn):
           'book_edit.html',
           book=new_book,
           msg="Book Exists. Submit to make another copy.",
-          force_create=True
+          add_url=ADD_SCAN_REDIR,
+          loan_url=BORROW_SCAN_REDIR,
       )
     else:
       new_book.put()
@@ -100,23 +108,44 @@ def add_from_isbn(isbn):
           'book_edit.html',
           book=new_book,
           msg="Book Added!",
+          add_url=ADD_SCAN_REDIR,
+          loan_url=BORROW_SCAN_REDIR,
       )
   else:
     logging.error("Failed to fetch book data: %s" % r.content)
     return render_template(
         'book_edit.html',
-        book=new_book,
+        book=model.Book(isbn=isbn),
         msg="Failed to fetch book data. please fill in the form manually.",
+        add_url=ADD_SCAN_REDIR,
+        loan_url=BORROW_SCAN_REDIR,
     )
 
-@app.route('/borrow/<isbn>', methods=['GET'])
+@app.route('/borrow/<isbn_or_key>', methods=['GET'])
+def borrow_dispatch(isbn_or_key):
+  #TODO: improve isbn matching.
+  if len(isbn_or_key) > 13:
+    #not an isbn.
+    return borrow_by_key(isbn_or_key)
+  else:
+    return borrow_by_isbn(isbn_or_key)
+
+def borrow_by_key(key):
+  book_key = ndb.Key(urlsafe=key)
+  loan = model.Loan.from_book(book_key.get())
+  loan.loaned_to = users.get_current_user().email()
+  loan.put()
+  logging.warn(loan.key.urlsafe())
+  return redirect("/loan/%s" % loan.key.urlsafe())
+
 def borrow_by_isbn(isbn):
+  # Borrow may be called with an isbn, or a book key.
   q = model.Book.query(
     model.Book.isbn == isbn)
   results = q.fetch(2)
   logging.warn(results)
   if len(results) == 0:
-    #book not found. add it?
+    #book not found. add it.
     return add_from_isbn(isbn)
   if len(results) == 1:
     # only one copy. loan it.
@@ -125,9 +154,8 @@ def borrow_by_isbn(isbn):
     loan.put()
     logging.warn(loan.key.urlsafe())
     return redirect("/loan/%s" % loan.key.urlsafe())
-    #return edit_loan(loan.key.urlsafe())
   else:
-    return "not implemented. yet."
+    return "support for multiple copies not implemented."
     # multiple copies known, pick one.
 
 @app.route('/loan/<key>')
@@ -137,6 +165,8 @@ def edit_loan(key):
   loan = ndb.Key(urlsafe=key).get()
   return render_template(
       'loan_edit.html',
+      add_url=ADD_SCAN_REDIR,
+      loan_url=BORROW_SCAN_REDIR,
       loan=loan)
 
 @app.route('/loan/<key>', methods=['POST'])
@@ -160,7 +190,23 @@ def show_books():
       'list_books.html',
       my_books=my_books,
       books=books,
+      add_url=ADD_SCAN_REDIR,
+      loan_url=BORROW_SCAN_REDIR,
   )
+
+@app.route('/loans')
+def show_loans():
+  user_email = users.get_current_user().email()
+  to_me = model.Loan.to_user(user_email)
+  from_me = model.Loan.from_user(user_email)
+  logging.debug(to_me.fetch(1))
+  logging.debug(from_me.fetch(1))
+  return render_template(
+      'loans_list.html',
+      add_url=ADD_SCAN_REDIR,
+      loan_url=BORROW_SCAN_REDIR,
+      loans_to=to_me,
+      loans_from=from_me)
 
 def fetch_details_from_isbn(isbn):
   """Call the google books api with an isbn, to return a dict of metadata."""
--- model.py
+++ model.py
@@ -11,14 +11,40 @@ class Book(ndb.Model):
   author = ndb.StringProperty(repeated=True)
   description = ndb.StringProperty()
 
+  def is_available(self):
+    """A book is available if it is not currently loaned out."""
+    lq = Loan.query(
+        Loan.book == self.key,
+        Loan.is_returned == False)
+    if lq.count(limit=1) > 0:
+      return False
+    return True
+
 class Loan(ndb.Model):
   book = ndb.KeyProperty()
   # may be a user, or just a name.
   loaned_to = ndb.StringProperty()
   loaned_from = ndb.StringProperty()
-  note = ndb.TextProperty()
+  note = ndb.TextProperty(default='')
   start_date = ndb.DateProperty(auto_now_add=True)
   end_date = ndb.DateProperty()
+  is_returned = ndb.BooleanProperty(default=False)
+
+  @classmethod
+  def to_user(cls, email):
+    """query to return books loaned to the email provided."""
+    query = Loan.query(
+        Loan.loaned_to == email,
+        Loan.is_returned == False)
+    return query
+
+  @classmethod
+  def from_user(cls, email):
+    """query to return books loaned by the email provided."""
+    query = Loan.query(
+        Loan.loaned_from == email,
+        Loan.is_returned == False)
+    return query
 
   @classmethod
   def from_book(cls, book_obj):
